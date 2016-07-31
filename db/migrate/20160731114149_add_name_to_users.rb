ffca984786491a0b1ec3a2064ac31ccd53043fb3
堀瑞希 <mizukirara@icloud.com>
2016-07-31 20:45:36 +0900

    added column of users

---
+++ db/migrate/20160731114149_add_name_to_users.rb
@@ -0,0 +1,9 @@
+class AddNameToUsers < ActiveRecord::Migration
+  def change
+    add_column :users, :name, :string
+    add_column :users, :member, :string
+    add_column :users, :profile, :text
+    add_column :users, :works, :string
+    add_column :users, :avatar, :text
+  end
+end
