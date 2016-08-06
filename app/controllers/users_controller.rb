370dbe0901e9fa5707eec6567a430d8c99eea36e
堀瑞希 <mizukirara@icloud.com>
2016-08-06 10:06:58 +0900

    removed 'update'

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,6 @@ class UsersController < ApplicationController
   end
 
   def update
-    current_user.update(user_params)
     if current_user.update(user_params)
        redirect_to root_url, notice: "Updated Profile Successfully"
     else
