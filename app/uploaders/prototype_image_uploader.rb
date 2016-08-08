1280d4b836e34d867bcf8783a779579c84935105
堀瑞希 <mizukirara@icloud.com>
2016-08-08 23:51:08 +0900

    added upholder

---
+++ app/uploaders/prototype_image_uploader.rb
@@ -0,0 +1,11 @@
+class PrototypeImageUploader < CarrierWave::Uploader::Base
+
+
+  storage :file
+
+  def store_dir
+    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+  end
+
+end
+
