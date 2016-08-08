8ba5197324a63171bc7a7d618e3425faed847e22
堀瑞希 <mizukirara@icloud.com>
2016-08-08 21:37:41 +0900

    create prototype_image table

--- db/migrate/20160806214059_create_prototype_images.rb
+++ db/migrate/20160806214059_create_prototype_images.rb
@@ -1,8 +1,10 @@
 class CreatePrototypeImages < ActiveRecord::Migration
   def change
     create_table :prototype_images do |t|
-
-      t.timestamps null: false
+      t.text        :content
+      t.references  :prototype, index: true, foreign_key: true
+      t.integer     :status
+      t.timestamps  null: false
     end
   end
 end
