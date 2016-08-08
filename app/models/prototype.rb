e4809ea3c3c65d6fda23fae6a05f8f4685b51dee
堀瑞希 <mizukirara@icloud.com>
2016-08-08 22:18:15 +0900

    added validates

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -2,4 +2,5 @@ class Prototype < ActiveRecord::Base
   has_many prototype_images
   belongs_to user
   accepts_nested_attributes_for :prototype_images
+  validates :title, :catchcopy, :concept, presence: true
 end
