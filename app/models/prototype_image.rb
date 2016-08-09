44fed61f6bc290b6150d7901d9f3792b19271875
堀瑞希 <mizukirara@icloud.com>
2016-08-09 12:18:14 +0900

    fixed models

--- app/models/prototype_image.rb
+++ app/models/prototype_image.rb
@@ -2,6 +2,7 @@ class PrototypeImage < ActiveRecord::Base
 
   belongs_to :prototype
   mount_uploader :content, PrototypeImageUploader
+  validates_presence_of :content
 
 end
 
--- app/uploaders/prototype_image_uploader.rb
+++ app/uploaders/prototype_image_uploader.rb
@@ -7,5 +7,6 @@ class PrototypeImageUploader < CarrierWave::Uploader::Base
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
+
 end
 
