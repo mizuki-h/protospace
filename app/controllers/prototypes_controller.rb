9acc4cdd56512ebf4416c5649593bd1287a8680c
堀瑞希 <mizukirara@icloud.com>
2016-08-15 20:43:21 +0900

    fixed show-action

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -5,7 +5,7 @@ class PrototypesController < ApplicationController
   end
 
   def show
-    @prototypes = Prototype.find(params[:id])
+    @prototype = Prototype.find(params[:id])
   end
 
   def new
