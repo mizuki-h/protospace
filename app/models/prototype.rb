95292ef36b0f469a91a3ef0603ac631b8ddfd69a
堀瑞希 <mizukirara@icloud.com>
2016-08-18 17:33:58 +0900

    added "dependent: :delete_all"

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,5 +1,5 @@
 class Prototype < ActiveRecord::Base
-  has_many :prototype_images
+  has_many :prototype_images, dependent: :delete_all
   belongs_to :user
   accepts_nested_attributes_for :prototype_images
   validates :title, :catchcopy, :concept, presence: true
