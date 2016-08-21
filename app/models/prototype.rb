9513cc574eb3d403f2522462d7bbdf110fd7aaac
堀瑞希 <mizukirara@icloud.com>
2016-08-21 17:17:33 +0900

    fixed models

--- app/models/prototype.rb
+++ app/models/prototype.rb
@@ -1,6 +1,6 @@
 class Prototype < ActiveRecord::Base
   has_many :prototype_images, dependent: :delete_all
-  has_many :likes
+  has_many :likes, dependent: :destroy
   belongs_to :user, dependent: :destroy
   accepts_nested_attributes_for :prototype_images
   validates :title, :catchcopy, :concept, presence: true
