6ab57ca6b544a8e4b60b2c08d3fd1ce612f0e3e7
堀瑞希 <mizukirara@icloud.com>
2016-08-22 17:30:31 +0900

    fixed like_controller

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -4,11 +4,12 @@ before_action :find_prototype ,only:[:create, :destroy]
 
   def create
     @like = @prototype.likes.create(user_id: current_user.id)
-    @prototype = Prototype.find(params[:prototype_id])
+    @prototype.reload
   end
 
   def destroy
     @prototype.likes.find_by(params[:user_id]).destroy
+    @prototype.reload
   end
 
   private
