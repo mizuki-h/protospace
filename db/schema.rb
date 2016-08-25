dda5996ce4b8a1d6fc89c119e60f2afebe35fd27
堀瑞希 <mizukirara@icloud.com>
2016-08-26 00:20:36 +0900

    added column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160823140211) do
+ActiveRecord::Schema.define(version: 20160825001612) do
 
   create_table "comments", force: :cascade do |t|
     t.integer  "user_id",      limit: 4
@@ -45,13 +45,14 @@ ActiveRecord::Schema.define(version: 20160823140211) do
   add_index "prototype_images", ["prototype_id"], name: "index_prototype_images_on_prototype_id", using: :btree
 
   create_table "prototypes", force: :cascade do |t|
-    t.string   "title",      limit: 255
-    t.string   "catchcopy",  limit: 255
-    t.text     "concept",    limit: 65535
-    t.integer  "user_id",    limit: 4
+    t.string   "title",         limit: 255
+    t.string   "catchcopy",     limit: 255
+    t.text     "concept",       limit: 65535
+    t.integer  "user_id",       limit: 4
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "count",      limit: 4,     default: 0
+    t.integer  "count",         limit: 4,     default: 0
+    t.integer  "comment_count", limit: 4,     default: 0
   end
 
   add_index "prototypes", ["user_id"], name: "index_prototypes_on_user_id", using: :btree
