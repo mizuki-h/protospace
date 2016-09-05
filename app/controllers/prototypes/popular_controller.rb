3d4aad118c65586a515a28fdd35d697277198ab9
堀瑞希 <mizukirara@icloud.com>
2016-09-05 11:00:45 +0900

    fixed controller

--- app/controllers/prototypes/popular_controller.rb
+++ app/controllers/prototypes/popular_controller.rb
@@ -1,4 +1,4 @@
-class PopularController < ApplicationController
+class Prototypes::PopularController < ApplicationController
   def index
     @prototypes = Prototype.includes(:user).order("count DESC")
   end
