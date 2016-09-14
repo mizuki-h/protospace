2b06a83d8430d6e5b2d87c639385d87774a1469d
堀瑞希 <mizukirara@icloud.com>
2016-09-14 13:51:04 +0900

    added js function

--- app/assets/javascripts/preview.js
+++ app/assets/javascripts/preview.js
@@ -16,3 +16,38 @@ $(function(){
   });
 });
 
+$(function(){
+  $('.input_sub_img').change(function(){
+    if (!this.files.length) {
+       return;
+     }
+
+    var selectedFile = this.files[0];
+    var fileReader = new FileReader();
+
+    fileReader.onload = function(event){
+      var loadedImageUri = event.target.result;
+      $('.review_sub_img').html('<img src="' + loadedImageUri + '">');
+    };
+
+    fileReader.readAsDataURL(selectedFile);
+  });
+});
+
+$(function(){
+  $('.input_sub02_img').change(function(){
+    if (!this.files.length) {
+       return;
+     }
+
+    var selectedFile = this.files[0];
+    var fileReader = new FileReader();
+
+    fileReader.onload = function(event){
+      var loadedImageUri = event.target.result;
+      $('.review_sub02_img').html('<img src="' + loadedImageUri + '">');
+    };
+
+    fileReader.readAsDataURL(selectedFile);
+  });
+});
